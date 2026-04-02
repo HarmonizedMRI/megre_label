779acd69bddd57bdf7da2d452f1bf280193d15a3
berkin <37940775+berkinbilgic@users.noreply.github.com>
2026-04-02 14:50:33 -0400

    Refactor path settings and clean up code
    Updated paths and filenames for sequence writing. Removed commented-out code for data reconstruction and added necessary comments.

--- script_writeGradientEcho3D_label_spoil_v1.m
+++ script_writeGradientEcho3D_label_spoil_v1.m
@@ -5,9 +5,8 @@
 clear
 restoredefaultpath
 
-addpath /cluster/berkin/berkin/Matlab_Code_New/PULSEQ/QSM_harmonization/pulseq-1.5.1/matlab/
-% addpath /cluster/berkin/berkin/Matlab_Code_New/PULSEQ/QSM_harmonization/pulseq-1.4.2/matlab/
-addpath /autofs/cluster/berkin/xingwang/share/seqeyes/
+% here add pulseq matlab directory to path e.g.
+addpath pulseq-1.5.1/matlab/
 
 % Define FOV and resolution
 fov = [264e-3 184e-3 144e-3];    
@@ -21,7 +20,6 @@ adc_dwell = 15e-6;
 Tread = adc_dwell * Nx;
 
 disp(['Tread ', num2str(Tread*1e3), ' ms'])
-% Tread = 4e-3;     % adc duration
 
 Ndummy = 50;
 
@@ -328,7 +326,7 @@ fprintf('Sequence ready\n');
 %% check whether the timing of the sequence is correct
 % -------------------------------------------------------------------------
 
-use_v141 = 1;
+use_v141 = 0;
 
 [ok, error_report] = seq.checkTiming;
 
@@ -351,17 +349,15 @@ seq.setDefinition('kSpaceCenterLine', centerLineIdx-1) ;
 seq.setDefinition('PhaseResolution', phaseResolution) ;
 
 if use_v141
-    seq.write_v141('gre3d_test_label_spoil_v0_v141.seq');
+    seq.write_v141('gre3d_test_label_spoil_v141.seq');
 else
-    seq.write('gre3d_test_lable.seq');
+    seq.write('gre3d_test_label_spoil.seq');
 end
 
 
 % -------------------------------------------------------------------------
-%% plot sequence
+%%
 % -------------------------------------------------------------------------
-
-seqeyes(seq)
 
 % === Compute and Display Total Scan Time ===
 total_time_sec = seq.duration();
@@ -376,290 +372,3 @@ disp('Sequence successfully generated!');
 fprintf('Total Scan Time: %d min %.1f sec\n', minutes, seconds);
 fprintf('Total Dummies: %d\n', Ndummy);
 disp('-----------------------------------------');
-
-
-
-
-% -------------------------------------------------------------------------
-%% Reconstruct 3D Multi-Echo GRE Pulseq Data with GRAPPA
-% Requires mapVBVD to be in your MATLAB path.
-% -------------------------------------------------------------------------
-
-addpath(genpath('/cluster/berkin/berkin/Matlab_Code_New/LIBRARY/'))
-
-% 1. Load the raw data using mapVBVD
-data_path = '/autofs/space/marduk_001/users/berkin/2026_03_31_bay5_phantom_qsm_harmonization/';
-
-filename = 'meas_MID00234_FID12436_pulseq151fix_gre3d_test_lable'; % Replace with your .dat file
-
-disp('Loading data with mapVBVD...');
-twix = mapVBVD2([data_path, filename, '.dat']);
-
-% Handle multi-raid files (often the actual data is the last cell)
-if iscell(twix)
-    twix = twix{end};
-end
-
-% 2. Extract k-space data
-% mapVBVD default squeezed dimensions are usually: [Col, Cha, Lin, Par, Eco]
-% Col = Nx (Readout)
-% Cha = Nc (Coils)
-% Lin = Ny (Phase Encoding Y)
-% Par = Nz (Partition/Phase Encoding Z)
-% Eco = Ne (Echoes)
-disp('Extracting k-space...');
-
-tic
-    kSpace = twix.image(); 
-    kSpace_ref = twix.refscan(); 
-toc
-
-kSpace = squeeze(kSpace); 
-kSpace_ref = squeeze(kSpace_ref); 
-
-% -------------------------------------------------------------------------
-%%
-% -------------------------------------------------------------------------
-
-kSpace = permute(kSpace, [1, 3, 4, 2, 5]);
-[~, ~, ~, Nc, Ne] = size(kSpace);
-
-kSpace_ref = permute(kSpace_ref, [1, 3, 4, 2, 5]);
-img_ref = ifft3call(kSpace_ref);    
-
-
-% -------------------------------------------------------------------------
-%% Reorder dimensions to [Nx, Ny, Nz, Nc, Ne] for easier mapping
-% From [Col, Cha, Lin, Par, Eco] -> [1, 3, 4, 2, 5]
-% -------------------------------------------------------------------------
-
-% Sequence Parameters (Must match your Pulseq script)
-R = 2; 
-acs_lines = 32; 
-
-Ny = 184;
-ky_indices_acs = 1+Ny/2-acs_lines/2:Ny/2+acs_lines/2;
-
-kSpace_all = kSpace;
-
-kSpace_all(:,ky_indices_acs,:,:,:) = kSpace_ref; %
-
-kSpace_all = padarray(kSpace_all, rem(size(kSpace_all(:,:,:,1,1)),2), 'post');
-
-[Nx,Ny,Nz] = size(kSpace_all(:,:,:,1,1));  % update k-space size after zero padding to even size
-
-img_zf = ifft3call(kSpace_all);
-
-for t = 1:Ne
-    imagesc3d2( rsos(kSpace_all(:,:,:,:,t),4), s(img_zf)/2, t, [90,90,90], [0.,1e-3]), setGcf(.6)
-    imagesc3d2( rsos(img_zf(:,:,:,:,t),4), s(img_zf)/2, 10+t, [90,90,0], [0.,5e-4]), setGcf(.6)
-end
-
-
-%--------------------------------------------------------------------------
-%% SVD coil compression: use 1st chan as body coil reference
-%--------------------------------------------------------------------------
-
-num_chan = 32;
-select_acs_echo = 1;
-
-[img_ref_svd, cmp_mtx] = svd_compress3d(img_ref(:,:,:,:,select_acs_echo), num_chan, 1);
-
-rmse(rsos(img_ref_svd,4),rsos(img_ref(:,:,:,:,select_acs_echo),4))
-
-% apply svd
-kSpace_all_svd = zeross([Nx,Ny,Nz,num_chan,Ne]);
-
-for t = 1:Ne
-    tmp = svd_apply3d(kSpace_all(:,:,:,:,t),cmp_mtx);
-    kSpace_all_svd(:,:,:,:,t) = tmp; 
-end
-
-rmse(rsos(kSpace_all_svd,4), rsos(kSpace_all,4))
-
-
-%--------------------------------------------------------------------------
-%% grappa: recon single slice
-%--------------------------------------------------------------------------
-
-addpath '/autofs/cluster/berkin/berkin/Matlab_Code_New/TOOLBOXES/Espirit_matlab_only_toolbox/utils'
-
-substitute_acs = 1;
-
-Ry = 2;
-Rz = 1;
-
-
-lambda_percent = 1e-3;      % percentage of sigma_min to use as regularizer
-
-num_acs = floor([size(kSpace_ref,2), Nz] / 2) * 2 - 2;
-kernel_size = [3,3];        % odd kernel size
-
-% espirit parameters
-num_acs_espirit = acs_lines;
-kernel_size_espirit = [6,6];
-eigen_thresh = 0.5;
-
-k_hybrid = ifftc(kSpace_all_svd,1);
-
-Img_Grappa_rsos = zeross([Nx,Ny,Nz,Ne]);
-Img_Grappa_combo = zeross([Nx,Ny,Nz,Ne]);
-
-tic
-for slc_select = 132%1:Nx
-    disp(slc_select)
-
-    k_hybrid_slc = sq(k_hybrid(slc_select,:,:,:,:));
-
-    k_hybrid_acs = zeross(size(k_hybrid_slc));
-    k_hybrid_acs(1+end/2-num_acs(1)/2:end/2+num_acs(1)/2, 1+end/2-num_acs(2)/2:end/2+num_acs(2)/2,:,:) = ...
-            k_hybrid_slc(1+end/2-num_acs(1)/2:end/2+num_acs(1)/2, 1+end/2-num_acs(2)/2:end/2+num_acs(2)/2,:,:);
-
-    % espirit
-    [maps, weights] = ecalib_soft( k_hybrid_acs(:,:,:,select_acs_echo), num_acs_espirit, kernel_size_espirit, eigen_thresh );
-
-    receive_slc = dot_mult(maps, weights >= eigen_thresh);
-
-
-    Im_Grappa = zeros([Ny, Nz, Nc, Ne]);    
-    Im_Grappa_combo = zeros([Ny, Nz, Ne]);    
-    
-    for t = 1:Ne
-        Im_Grappa(:,:,:,t) = grappa_gfactor_2d_jvc3( k_hybrid_slc(:,:,:,t), k_hybrid_acs(:,:,:,t), Ry, Rz, num_acs, kernel_size, lambda_percent, substitute_acs );
-    end
-    
-    % substitute acs
-    k_Grappa = fft2call(Im_Grappa);
-    k_Grappa(ky_indices_acs,:,:,:) = k_hybrid_slc(ky_indices_acs,:,:,:);
-    Im_Grappa = ifft2call(k_Grappa);
-
-    for t = 1:Ne
-        Im_Grappa_combo(:,:,t) = coil_combine(Im_Grappa(:,:,:,t),receive_slc,3);
-    end
-
-    Img_Grappa_rsos(slc_select,:,:,:) = rsos(Im_Grappa,3);
-    Img_Grappa_combo(slc_select,:,:,:) = Im_Grappa_combo;
-end
-toc
-
-
-close all
-
-mosaic( rsos(ifft2call(k_hybrid_acs),3), 1, 5, 20, '', [0,3e-4], 90 ), setGcf(.7)
-mosaic( rsos(ifft2call(k_hybrid_slc),3), 1, 5, 21, '', [0,3e-4], 90 ), setGcf(.7)
-mosaic( Img_Grappa_rsos(slc_select,:,:,:), 1, 5, 22, '', [0,3e-4], 90 ), setGcf(.7)
-
-mosaic( Img_Grappa_combo(slc_select,:,:,:), 1, 5, 23, '', [0,3e-4], 90 ), setGcf(.7)
-mosaic( angle(Img_Grappa_combo(slc_select,:,:,:)), 1, 5, 24, '', [-pi,pi], 90 ), setGcf(.7)
-
-mosaic( rsos(k_hybrid_acs,3), 1, 5, 30, '', [0,1e-4] )
-mosaic( rsos(k_hybrid_slc,3), 1, 5, 31, '', [0,1e-4] )
-
-
-
-%--------------------------------------------------------------------------
-%% grappa: recon single slice with parfor
-%--------------------------------------------------------------------------
-
-% --- 1. Parallel Pool Setup ---
-num_cores = 16; % Specify the number of cores you want to use
-
-% Check if a parallel pool already exists
-poolobj = gcp('nocreate'); 
-
-% If no pool exists, or if the current pool has the wrong number of workers, start a new one
-if isempty(poolobj)
-    parpool('local', num_cores);
-elseif poolobj.NumWorkers ~= num_cores
-    disp('Restarting parallel pool with the requested number of cores...');
-    delete(poolobj);
-    parpool('local', num_cores);
-end
-% ------------------------------
-
-substitute_acs = 1;
-
-Ry = 2;
-Rz = 1;
-
-lambda_percent = 1e-3;      % percentage of sigma_min to use as regularizer
-
-num_acs = floor([size(kSpace_ref,2), Nz] / 2) * 2 - 2;
-kernel_size = [3,3];        % odd kernel size
-
-% espirit parameters
-num_acs_espirit = acs_lines;
-kernel_size_espirit = [6,6];
-eigen_thresh = 0.5;
-
-k_hybrid = ifftc(kSpace_all,1);
-
-Img_Grappa_rsos = zeros([Nx, Ny, Nz, Ne]);
-Img_Grappa_combo = zeros([Nx, Ny, Nz, Ne]);
-
-tic
-parfor slc_select = 1:Nx
-    % disp() inside parfor prints out of order, which is normal
-    disp(['Reconstructing slice: ', num2str(slc_select)]);
-
-    % Extract slice using squeeze to ensure proper slicing and save RAM
-    k_hybrid_slc = sq(k_hybrid(slc_select,:,:,:,:));
-
-    k_hybrid_acs = zeross(size(k_hybrid_slc));
-    k_hybrid_acs(1+end/2-num_acs(1)/2:end/2+num_acs(1)/2, 1+end/2-num_acs(2)/2:end/2+num_acs(2)/2,:,:) = ...
-            k_hybrid_slc(1+end/2-num_acs(1)/2:end/2+num_acs(1)/2, 1+end/2-num_acs(2)/2:end/2+num_acs(2)/2,:,:);
-        
-    % espirit
-    [maps, weights] = ecalib_soft( k_hybrid_acs(:,:,:,select_acs_echo), num_acs_espirit, kernel_size_espirit, eigen_thresh );
-
-    receive_slc = dot_mult(maps, weights >= eigen_thresh);
-
-    receive_slc = abs(receive_slc) .* exp(1i * angle( receive_slc .* repmat(conj(receive_slc(:,:,1)), [1,1,num_chan]) ));
-
-    % Temporary variable inside the parfor loop for the current slice
-    Im_Grappa = zeros([Ny, Nz, Nc, Ne]);    
-    Im_Grappa_combo = zeros([Ny, Nz, Ne]);    
-    
-    for t = 1:Ne
-        % Perform GRAPPA on the current echo/time-point
-        Im_Grappa(:,:,:,t) = grappa_gfactor_2d_jvc3(...
-            k_hybrid_slc(:,:,:,t), ...
-            k_hybrid_acs(:,:,:,t), ...
-            Ry, Rz, num_acs, kernel_size, lambda_percent, substitute_acs);
-    end
-
-    % substitute acs
-    k_Grappa = fft2call(Im_Grappa);
-    k_Grappa(ky_indices_acs,:,:,:) = k_hybrid_slc(ky_indices_acs,:,:,:);
-    Im_Grappa = ifft2call(k_Grappa);
-
-    for t = 1:Ne
-        Im_Grappa_combo(:,:,t) = coil_combine(Im_Grappa(:,:,:,t),receive_slc,3);
-    end
-
-
-    % Root sum of squares over the coil dimension (dim 3)
-    Img_Grappa_rsos(slc_select, :, :, :) = rsos(Im_Grappa, 3);
-
-    % espirit coil combination
-    Img_Grappa_combo(slc_select,:,:,:) = Im_Grappa_combo;
-end
-toc
-
-% --- 2. Release Cores ---
-disp('Shutting down parallel pool and releasing cores...');
-delete(gcp('nocreate'));
-% ------------------------
-
-for t = 1:Ne
-    imagesc3d2( Img_Grappa_rsos(:,:,:,t), s(Img_Grappa_rsos)/2, t, [90,90,90], [0.,5e-4]), setGcf(.6)
-    imagesc3d2( Img_Grappa_combo(:,:,:,t), s(Img_Grappa_rsos)/2, 10+t, [90,90,90], [0.,5e-4]), setGcf(.6)
-    imagesc3d2( angle(Img_Grappa_combo(:,:,:,t)), s(Img_Grappa_rsos)/2, 20+t, [90,90,90], [-pi,pi]), setGcf(.6)
-end
-
-tic
-    save([data_path, filename, 'Img_Grappa_rsos.mat'], 'Img_Grappa_rsos', '-v7.3')
-    save([data_path, filename, 'Img_Grappa_combo.mat'], 'Img_Grappa_combo', '-v7.3')
-toc
-
-genNiigz(abs(Img_Grappa_combo(:,:,:,end)),[1,1,1],[data_path, filename, 'Img_Grappa_combo.nii'])
